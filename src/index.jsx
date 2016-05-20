95af4cd56f2151a7efe607ee5f193a0d9a36c203
Bobby Seidensticker <bseidensticker@gmail.com>
2016-05-20 16:23:50 -0700

    check

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as firebase from 'firebase';
+
+
+
+
+
+
+let root;
 
 var Home = React.createClass({
+  getInitialState: function() {
+    return {count: 0};
+  },
+
+  onClick: function() {
+    root.child('huh').push('ass').then(() => {
+      this.setState({count: this.state.count + 1});
+    });
+  },
+
   render: function() {
     return (
       <div>
         <h1>Welcome to my lifting helper!</h1>
         <div>Pick a workout</div>
+        <input type='button' value='shit!' onClick={this.onClick} />
+        <div>{this.state.count}</div>
       </div>
     );
   },
 });
 
 function init() {
-  let root = document.getElementById('root');
+  // Initialize Firebase
+  var config = {
+    authDomain: "lift-669c1.firebaseapp.com",
+    databaseURL: "https://lift-669c1.firebaseio.com",
+    storageBucket: "lift-669c1.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  root = firebase.database().ref();
 
   window.addEventListener('load', function() {
     ReactDOM.render(
       React.createElement(Home, {}),
-      root
+      document.getElementById('root')
     );
   });
 }
